3eeb07d38ff0ab497bd8e98ca4df52176a359b35
aravindmj97 <aravindmj97@gmail.com>
2018-09-23 20:53:44 +0530

    Fixed crash in M<devices, Added on Boot Start

--- app/src/main/java/com/a4dotsinc/dynamicwalls/TimeReceiver.java
+++ app/src/main/java/com/a4dotsinc/dynamicwalls/TimeReceiver.java
@@ -1,22 +1,28 @@
 package com.a4dotsinc.dynamicwalls;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class TimeReceiver extends BroadcastReceiver {
@@ -28,6 +34,21 @@ public class TimeReceiver extends BroadcastReceiver {
 
         final SharedPreferences sharedPref = context.getSharedPreferences(
                 context.getString(R.string.active_switches), Context.MODE_PRIVATE);
+
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+                Intent alarmIntent = new Intent(context, TimeReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+
+                AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                int interval = sharedPref.getInt(context.getString(R.string.time_interval), 10);
+
+                manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()+10000,
+                        1000 * 60 * interval, pendingIntent);
+                Toast.makeText(context, "Dynamic Walls Activated", Toast.LENGTH_SHORT).show();
+        }
 
         this.context = context;
         imageLoader = ImageLoader.getInstance();
@@ -58,15 +79,19 @@ public class TimeReceiver extends BroadcastReceiver {
                     try{
                         if (sharedPref.getInt(context.getString(R.string.blur_on), 0) == 1){
                             wallpaperManager.setBitmap(blutIt(loadedImage, 2f));
-                            wallpaperManager.setBitmap((blutIt(loadedImage, 2f)), null, true, WallpaperManager.FLAG_LOCK);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                wallpaperManager.setBitmap((blutIt(loadedImage, 2f)), null, true, WallpaperManager.FLAG_LOCK);
+                            }
                         }
                         else{
                             wallpaperManager.setBitmap(loadedImage);
-                            wallpaperManager.setBitmap(loadedImage, null, true, WallpaperManager.FLAG_LOCK);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                wallpaperManager.setBitmap(loadedImage, null, true, WallpaperManager.FLAG_LOCK);
+                            }
                         }
                         Log.d("Wall Changed", "newWallAt: ");
                         SharedPreferences.Editor editor = sharedPref.edit();
-                        editor.putString(context.getString(R.string.last_sync), android.text.format.DateFormat.format("hh:mm:aa dd-MM-yyyy", new Date()).toString());
+                        editor.putString(context.getString(R.string.last_sync), DateFormat.format("hh:mm:aa dd-MM-yyyy", new Date()).toString());
                         editor.commit();
                     }catch (Exception e){
                         e.printStackTrace();
